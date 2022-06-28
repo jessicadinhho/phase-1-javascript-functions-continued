91f634035fe934bfa183aabdca8bd6f9ebd2f2da
Jessica Ho <jessicadinhho@gmail.com>
2022-06-28 08:34:00 -0700

    commit tree 985d5f324b7459f74d8af33f4494d437180bd46f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+};
+const mondayWork = function (work = "go to the office") {
+    return `This Monday, I will ${work}.`
+};
+function wrapAdjective(nerd="Special") {
+        return function (puto="*") {
+            return `You are ${nerd}${puto}${nerd}!`
+        }
+}
+        
+    
